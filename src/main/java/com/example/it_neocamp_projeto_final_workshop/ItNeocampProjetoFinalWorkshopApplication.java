8b17943ba72205cc60f761646ee53ac978607310
sitavares_meli <silvio.tavares@mercadolivre.com>
2026-05-26 16:19:32 -0300

    feat: adicionando setup inicial do projeto

---
+++ src/main/java/com/example/it_neocamp_projeto_final_workshop/ItNeocampProjetoFinalWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.it_neocamp_projeto_final_workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItNeocampProjetoFinalWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItNeocampProjetoFinalWorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/it_neocamp_projeto_final_workshop/ItNeocampProjetoFinalWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.it_neocamp_projeto_final_workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItNeocampProjetoFinalWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
